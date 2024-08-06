845791e927a61b172393ea8520bcc0db81b7e21a
chandra9499 <chandrashekar.6569@gmail.com>
2024-08-07 00:09:43 +0530

    created entitys Department and Employee

---
+++ ModelLayer/Model/Entity/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Model.Entity
+{
+    public class Department
+    {
+        [Key]
+        public int DeptId { get; set; }
+        [Required(ErrorMessage ="department name is requiered")]
+        [Column("dept_name")]
+        public string DepartmentName { get; set; }
+        [Required(ErrorMessage = "department location is requiered")]
+        [Column("dept_loc")]
+        public string DepartmentLocation { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+}
---
+++ ModelLayer/Model/Entity/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Model.Entity
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required(ErrorMessage = "Employee Name is requiered")]
+        [StringLength(50)]
+        public string EmployeeName { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public double EmployeeSalary { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        public String Note { get; set; }
+        public List<Department> Departments { get; set; }
+    }
+}
